1c8e5ea60290a88056c2b80dd0411a4fd3863ed0
Rilind Hoxhaj <rilind@Rilinds-MBP.fritz.box>
2022-08-07 19:14:43 +0200

    feat: show restaurants

---
+++ src/components/RestaurantsList/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { restaurants } from '../../utils/restaurant';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import CircleIcon from '@mui/icons-material/Circle';
+
+const RestaurantList = () => {
+  return (
+    <div>
+      <p class="m-2">Nearby Restaurants</p>
+      <div class="lg:grid grid-cols-3 gap-4 m-2">
+        {restaurants.map((restaurant) => (
+          <div
+            key={restaurant.id}
+            class="max-w-sm md:mt-2 bg-white rounded-lg border border-gray-200 dark:border-gray-300"
+          >
+            <img
+              class="rounded-t-lg object-cover h-48 w-96"
+              src={restaurant.image}
+              alt=""
+            />
+            <div class="p-5">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                {restaurant.name}
+              </h5>
+              <div class="mb-3 flex font-normal text-gray-700 dark:text-gray-400">
+                <AccessTimeIcon />
+                <p class="ml-2">{restaurant.deliveryTime}</p>
+                <CircleIcon
+                  sx={{
+                    color: 'blue',
+                    fontSize: 5,
+                    alignSelf: 'center',
+                    marginLeft: '10px',
+                  }}
+                />
+                <p class="ml-2">{restaurant.price} min sum</p>
+              </div>
+              <div class="flex">
+                {restaurant.type.map((type) => (
+                  <p class="mr-2 bg-slate-300 rounded-2xl p-1">{type}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantList;
---
+++ src/utils/restaurant.js
@@ -0,0 +1,57 @@
+import burgers_and_pizza from '../assets/img/burgers_and_pizza.png';
+import japanese_sushi from '../assets/img/japanese_sushi.png';
+import kobe from '../assets/img/kobe.png';
+import ninja_sushi from '../assets/img/ninja_sushi.png';
+import sushi_house from '../assets/img/sushi_house.png';
+import sushi_master from '../assets/img/sushi_master.png';
+
+export const restaurants = [
+  {
+    id: 1,
+    name: 'Royal Sushi House',
+    type: ['🍣 Sushi'],
+    deliveryTime: '30-40min',
+    price: '$32',
+    image: sushi_house,
+  },
+  {
+    id: 2,
+    name: 'Burgers & Pizza',
+    type: ['🍔 Burger', '🍕 Pizza'],
+    deliveryTime: '40-60min',
+    price: '$24',
+    image: burgers_and_pizza,
+  },
+  {
+    id: 3,
+    name: 'Ninja Sushi',
+    type: ['🍣 Sushi'],
+    deliveryTime: '20-40min',
+    price: '$40',
+    image: ninja_sushi,
+  },
+  {
+    id: 4,
+    name: 'Sushi Master',
+    type: ['🍣 Sushi'],
+    deliveryTime: '60-70min',
+    price: '$49',
+    image: sushi_master,
+  },
+  {
+    id: 5,
+    name: 'Japanese Sushi',
+    type: ['🍣 Sushi'],
+    deliveryTime: '30-50min',
+    price: '$104',
+    image: japanese_sushi,
+  },
+  {
+    id: 6,
+    name: 'Kobe',
+    type: ['🍣 Sushi'],
+    deliveryTime: '20-30min',
+    price: '$57',
+    image: kobe,
+  },
+];
