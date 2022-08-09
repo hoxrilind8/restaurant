6031c9c4e0168f3f7a819f111d7905f242913fee
Rilind Hoxhaj <rilind@Rilinds-MBP.fritz.box>
2022-08-09 23:11:01 +0200

    fix: filter function based on filters

--- src/App.js
+++ src/App.js
@@ -3,26 +3,43 @@ import React, { useState } from 'react';
 import FilterButtons from './components/FilterButtons';
 import Navigation from './components/Navigation';
 import RestaurantList from './components/RestaurantsList';
-import { restaurants } from './utils/restaurant';
+import Cards from './components/Cards';
 
-function App() {
-  const [filteredRestaurant, setFilteredRestaurants] = useState(restaurants);
-  const filterRestaurant = (val) => {
-    const newRestaurants = restaurants.filter((newVal) => {
-      const type = newVal.type.map((type) => type);
-      for (let i = 0; i < type.length; i++) {
-        if (type[i] === val) return val;
-      }
-    });
-    setFilteredRestaurants(newRestaurants);
-  };
+import { restaurants } from './utils/restaurants';
+import { allFilters } from './utils/filters';
+
+export default function App() {
+  const [filters, setFilters] = useState(allFilters);
+
+  function selectFilter(filterId) {
+    const selectedFilterIndex = filters.findIndex(({ id }) => id === filterId);
+    const newFilters = [...filters];
+
+    if (newFilters[selectedFilterIndex].active) {
+      newFilters[selectedFilterIndex].active = false;
+      return setFilters(newFilters);
+    }
+
+    newFilters[selectedFilterIndex].active = true;
+    return setFilters(newFilters);
+  }
+
+  const activeFilters = filters.filter((filter) => filter.active);
+  const activeFiltersType = activeFilters.map(({ filterType }) => filterType);
+
+  const filteredRestaurants = restaurants.filter(({ type }) => {
+    return type.some((t) => activeFiltersType.includes(t));
+  });
+
+  const restaurantsList =
+    filteredRestaurants.length === 0 ? restaurants : filteredRestaurants;
+
   return (
-    <div>
+    <>
       <Navigation />
-      <FilterButtons filterRest={filterRestaurant} />
-      <RestaurantList restaurants={filteredRestaurant} />
-    </div>
+      <Cards />
+      <FilterButtons selectFilter={selectFilter} filters={filters} />
+      <RestaurantList restaurants={restaurantsList} />
+    </>
   );
 }
-
-export default App;
---
+++ src/utils/filters.js
@@ -0,0 +1,44 @@
+export const allFilters = [
+  {
+    id: 1,
+    name: 'Pizza',
+    filterType: 'pizza',
+    active: false,
+    icon: '🍕',
+  },
+  {
+    id: 2,
+    name: 'Burger',
+    filterType: 'burger',
+    active: false,
+    icon: '🍔',
+  },
+  {
+    id: 3,
+    name: 'BBQ',
+    filterType: 'bbq',
+    active: false,
+    icon: '🥩',
+  },
+  {
+    id: 4,
+    name: 'Sushi',
+    filterType: 'sushi',
+    active: false,
+    icon: '🍣',
+  },
+  {
+    id: 5,
+    name: 'Vegan',
+    filterType: 'vegan',
+    active: false,
+    icon: '🥦',
+  },
+  {
+    id: 6,
+    name: 'Desserts',
+    filterType: 'desserts',
+    active: false,
+    icon: '🧁',
+  },
+];
